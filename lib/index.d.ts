16f44cfc728b8f1cf920f5a513c62a59f33a7990
Aleksey Tarasenko <alekstar79@yandex.ru>
2026-03-20 17:46:57 +0400

    fix: removing unnecessary directories

--- lib/index.d.ts
+++
@@ -1,3 +0,0 @@
-import { Eccl, ImageFormat, Mask, Mode, QRCode, QROptions } from './types';
-export declare function makeQRCode(textOrOptions?: string | QROptions, options?: QROptions): QRCode;
-export type { Eccl, ImageFormat, Mode, Mask, QROptions };
